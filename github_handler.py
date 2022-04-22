fd0750a8f7506f6df149f9740b32df7e93a65848
Marcin Jarczewski <marcin.jarc@gmail.com>
2022-04-23 00:05:23 +0200

    get_info, authenticated endpoints created

--- github_handler.py
+++ github_handler.py
@@ -1,36 +1,46 @@
-import requests
-import json
+from fastapi import HTTPException
+from typing import Dict, List, Union, Any, Optional
 import os
+import json
+import requests
 
-
-def get_langs(repo, user: str, token: str):
-    link = repo['languages_url']
-    langs = requests.get(link, auth=(user, token))
+def get_langs(repo: Dict[str, Any], user: str = None, token: str = None) -> Dict[str, Optional[Dict[str, int]]]:
     response = {'langs': None}
+    
+    try:
+        link = repo['languages_url']
+    except KeyError as err:
+        print(err)
+        return response 
+    
+    langs = requests.get(link, auth=(user, token))
     if langs.ok:
         response['langs'] = langs.json()
 
     return response
 
 
-def get_repos(username, user, token):
+def get_repos(username: str, user: str = None, token: str = None) -> Dict[str, List[Any]]:
     data = requests.get(f'https://api.github.com/users/{username}/repos', auth=(user, token))
 
     response = {'repos': []}
     
     if data.ok:
         for repo in data.json():
-            repo = {
+            print(repo)
+            repo_dict = {
                 'name': repo['name'],
                 'langs': get_langs(repo, user, token)['langs']
             }
 
-            response['repos'].append(repo)
+            response['repos'].append(repo_dict)
+
+    print(response)
 
     return response
 
 
-def get_info(username, user, token):
+def get_info(username: str, user: str = None, token: str = None):
     data = requests.get(f'https://api.github.com/users/{username}', auth=(user, token))
 
     response = {}
@@ -41,20 +51,22 @@ def get_info(username, user, token):
         response['name'] = (data['name'])
         response['bio'] = (data['bio'])
         response['repos'] = get_repos(username, user, token)['repos']
-
+    else:
+        response = {"Error": "No such user exists"}
+        raise HTTPException(status_code=404)
     return response
 
 
-def save_to_json(filename, res):
+def save_to_json(filename: str, res: str):
     with open(f'{filename}.json', 'w') as f:
         json.dump(res, f, indent=2)
 
 
-def is_authenticated(user, token):
+def is_authenticated(user: str = None, token: str = None):
     
     r = requests.get(f'https://api.github.com/user', auth=(user, token))
 
-    return r.ok
+    return {"Response": "User not authenticated"}
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
+from github_handler import get_info, is_authenticated
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"Data": "Test"} 
+
+
+@app.get("/get-repos/{username}")
+def get_repos(username: str):
+    return get_info(username)
+
+@app.get("/authenticated")
+def authentication():
+    return is_authenticated()
